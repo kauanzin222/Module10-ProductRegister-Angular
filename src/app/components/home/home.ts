db43e76e2a698ddc7f4390ed4a9afcc0abcf526c
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2026-01-12 19:58:49 -0300

    add carousel a home

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './home.css',
 })
 export class Home {
+  images = ["imgExemplo1.png", "imgExemplo2.png", "imgExemplo3.png"];
+
+  constructor(config: NgbCarouselConfig) {
+		config.interval = 10000;
+		config.wrap = false;
+		config.keyboard = false;
+		config.pauseOnHover = false;
+	}
 }
